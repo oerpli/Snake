cdfd53bfecf8b988f5a4b56745f6ae11d3855e36
Abraham Hinteregger <oerpli@hotmail.com>
2018-03-19 23:15:10 +0100

    snake init

---
+++ GameBoard.py
@@ -0,0 +1,62 @@
+import random
+from Snake import *
+from Direction import *
+
+class GameBoard:
+	SIZE_X = 21
+	SIZE_Y = 21
+
+	def __init__(self):
+		self.Snake = Snake(GameBoard.SIZE_X // 2, GameBoard.SIZE_Y // 2)
+		self.Food = (0,0)
+		self.GenerateFood()
+		self.Grow = False
+	
+	def ClearConsole(self):
+		os.system('cls' if os.name == 'nt' else 'clear')
+
+	def Print(self):
+		# self.ClearConsole()
+		print(self)
+
+	def Step(self, dir = None):
+		(e,n,success) = self.Snake.Move(dir, self.Grow)
+		self.Grow = False
+		nf = None
+		if n == self.Food:
+			self.Grow = True
+			self.GenerateFood()
+			nf = self.Food
+		# Return information about new gamestate:
+		# e if a snakeblock is removed (coords)
+		# n if snake grows (coords)
+		# Bool if gamestate is still valid
+		# nf = None or coords of new food
+		return (e,n,success and self.IsValidPoint(n),nf)
+
+
+
+	def IsValidPoint(self,point):
+		if point[0] < 0 or point[1] < 0:
+			return False
+		if point[0] >= GameBoard.SIZE_X or point[1] >= GameBoard.SIZE_Y:
+			return False
+		return True
+
+	def SamplePoint(self):
+		while True:  # very slow if snake is long
+			x = random.randint(0, GameBoard.SIZE_X-1)
+			y = random.randint(0, GameBoard.SIZE_Y-1)
+			if not self.Snake.IsSnake((x, y)):
+				return (x, y)
+
+	def GenerateFood(self):
+		self.Food = self.SamplePoint()
+
+	def __str__(self): # printer for debugging in console
+		fields = [list("_"*GameBoard.SIZE_Y) for _ in range(GameBoard.SIZE_X)]
+		for (x, y) in self.Snake.GetCoordinates():
+			fields[y][x] = 'X'
+		(fX, fY) = self.Food
+		fields[fY][fX] = 'O'
+		return "\n".join(["".join(line) for line in reversed(fields)])
---
+++ Snake.py
@@ -0,0 +1,35 @@
+import random
+import os
+from enum import Enum
+from collections import deque
+from Direction import *
+
+class Snake:
+	INIT_SIZE = 5
+	SPEED = 1
+
+	def __init__(self, X, Y):
+		self.Direction = Direction.RIGHT
+		self.Segments = deque()
+		for i in range(Snake.INIT_SIZE):
+			self.Segments.appendleft((X-i, Y))
+
+	def Move(self, direction = None, grow=False):
+		if direction is not None:
+			self.Direction = direction
+		last = self.Segments.pop()
+		new = Direction.AddToPoint(last, self.Direction)
+		self.Segments.append(last)
+		if new in self.Segments:
+			return (None,new,False) # snake died
+		self.Segments.append(new)
+		if not grow:
+			end = self.Segments.popleft()
+		return (end,new,True) # step successful
+
+	def GetCoordinates(self):
+		for elems in self.Segments:
+			yield elems
+
+	def IsSnake(self, coords):
+		return coords in self.GetCoordinates()
